bec39e28b912575fe64c891e2571a9f88d0c1fd2
James Ly <jamesly97@yahoo.ca>
2018-10-22 20:35:31 -0400

    Add files via upload

--- question.java
+++ question.java
@@ -1,15 +1,16 @@
 /**
  * A silly simple program to output a 1 - 12 multiplication table.
- * Time space = O^2
+ * Time complexity = O^2
  * @author  - James Ly
  * @date    - 10.22.2018
  */
 
 public class question
 {
+	static int n = 12;
 	public static void main(String[] args) {
-    for (int i = 1; i <= 12; i++) {
-			for (int y = 1; y <= 12; y++) {
+    for (int i = 1; i <= n; i++) {
+			for (int y = 1; y <= n; y++) {
 				System.out.print(i * y + " ");
 			}
 			System.out.print("\n");
